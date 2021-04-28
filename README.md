75fd205f2477200fecf51cbbb39c89a9ed987f1c
oleg-ostanin <oleg.alex.ostanin@gmail.com>
2021-04-28 13:24:24 +0300

    created index mapping

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Install and run elasticsearch using this guide:
 
 https://www.elastic.co/guide/en/elasticsearch/reference/current/install-elasticsearch.html
+https://www.udemy.com/course/elasticsearch-7-and-elastic-stack/learn/lecture/14728824#overview 8 min
--- src/main/java/com/example/indexer/parser/RSStoElasticsearchDataLoaderImpl.java
+++ src/main/java/com/example/indexer/parser/RSStoElasticsearchDataLoaderImpl.java
@@ -4,18 +4,24 @@ package com.example.indexer.parser;
 import com.example.indexer.data.Channel;
 import com.example.indexer.data.Item;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.AnalyzeRequest;
 import org.elasticsearch.client.indices.AnalyzeResponse;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Reads data from RSS feed and loads it to elasticsearch.
@@ -27,7 +33,12 @@ public class RSStoElasticsearchDataLoaderImpl implements RSStoElasticsearchDataL
     private static final int PORT_9200 = 9200;
     private static final int PORT_9201 = 9201;
     private static final String NILS_TEST_INDEX = "nils_test_index";
+    private static final String PROPERTIES = "properties";
     private static final String TITLE = "title";
+    private static final String TYPE = "type";
+    private static final String TEXT = "text";
+    private static final String ANALYZER = "analyzer";
+    private static final String ENGLISH = "english";
     private static final String TITLE_TAGS = "title_tags";
     private static final String LINK = "link";
     private static final String PUB_DATE = "pubDate";
@@ -51,9 +62,71 @@ public class RSStoElasticsearchDataLoaderImpl implements RSStoElasticsearchDataL
 
         Channel channel = new RSSParser().parse();
 
+        createIndex();
+
         pushItems(channel.getItems());
 
         tearDown();
+    }
+
+    private void createIndex() throws IOException {
+        DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest(NILS_TEST_INDEX);
+
+        try {
+            AcknowledgedResponse deleteIndexResponse = client.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        CreateIndexRequest request = new CreateIndexRequest(NILS_TEST_INDEX);
+
+        Map<String, String> titleMap = new HashMap<>();
+        titleMap.put(TYPE, TEXT);
+        titleMap.put(ANALYZER, ENGLISH);
+
+        XContentBuilder builder = XContentFactory.jsonBuilder();
+        builder.startObject();
+        {
+            builder.startObject(PROPERTIES);
+            {
+                builder.startObject(TITLE);
+                {
+                    builder.field(TYPE, TEXT);
+                    builder.field(ANALYZER, ENGLISH);
+                }
+                builder.endObject();
+                builder.startObject(LINK);
+                {
+                    builder.field(TYPE, TEXT);
+                }
+                builder.endObject();
+                builder.startObject(PUB_DATE);
+                {
+                    builder.field(TYPE, TEXT);
+                }
+                builder.endObject();
+                builder.startObject(COMMENTS);
+                {
+                    builder.field(TYPE, TEXT);
+                }
+                builder.endObject();
+                builder.startObject(DESCRIPTION);
+                {
+                    builder.field(TYPE, TEXT);
+                }
+                builder.endObject();
+            }
+            builder.endObject();
+        }
+        builder.endObject();
+        request.mapping(builder);
+
+
+
+        CreateIndexResponse createIndexResponse = client.indices().create(request, RequestOptions.DEFAULT);
+
+        System.out.println(createIndexResponse);
     }
 
     private void pushItems(List<Item> items) throws IOException {
@@ -70,7 +143,7 @@ public class RSStoElasticsearchDataLoaderImpl implements RSStoElasticsearchDataL
         builder.startObject();
 
         builder.field(TITLE, item.getTitle());
-        builder.array(TITLE_TAGS, getTags(item.getTitle()));
+        //builder.array(TITLE_TAGS, getTags(item.getTitle()));
         builder.field(LINK, item.getLink().toString());
         builder.field(PUB_DATE, item.getPubDate());
         builder.field(COMMENTS, item.getComments());
